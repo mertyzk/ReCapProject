b75bc28f155e9c33534b15ababba86319269de7a
= <meyuzak@gmail.com>
2021-11-10 19:48:16 +0300

    Result

--- Business/Abstract/ICarService.cs
+++ Business/Abstract/ICarService.cs
@@ -12,11 +12,12 @@ namespace Business.Abstract
         List<Car> GetCarsByBrandId(int brandId);
         List<Car> GetCarsByColorId(int colorId);
         Car GetById(int carId);
+        List<CarDetailDto> GetCarDetails();
 
         void Add(Car car);
         void Update(Car car);
         void Delete(Car car);
 
-        List<CarDetailDto> GetCarDetails();
+
     }
 }
